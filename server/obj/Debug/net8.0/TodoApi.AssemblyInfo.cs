4630f4ebbbea0efd7961198e019d59179a72dcf7
Tamar-Gila <t0583249719@gmail.com>
2025-04-30 21:57:18 +0300

    הודעה המתארת את השינויים שביצעת

--- server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a158a69a5459ad12131f6506e3fc9caa90d5a1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d51eae89bfe5d183cab98794dfc4cd1ca9d2d3d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
