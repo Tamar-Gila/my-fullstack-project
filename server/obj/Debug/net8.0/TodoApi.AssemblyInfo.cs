330001e448861ca445951b4d9f72ee289ded839f
Tamar-Gila <t0583249719@gmail.com>
2025-05-04 21:04:41 +0300

    add 2

--- server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88334873b254c01e39f9a994e64666d9577c937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b2c92d5fb99b19cff27ac509291f313559e5428")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
